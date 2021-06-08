65da3fb9121058bb7a9c203fee574dba5c311e50
seondongpyo <seondongpyo@gmail.com>
2021-06-08 21:45:25 +0900

    fix: test 환경에서만 동작하도록 ActiveProfiles("test") 어노테이션 다시 추가

--- src/test/java/nextstep/subway/utils/DatabaseCleanup.java
+++ src/test/java/nextstep/subway/utils/DatabaseCleanup.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@ActiveProfiles("test")
 public class DatabaseCleanup implements InitializingBean {
     @PersistenceContext
     private EntityManager entityManager;
